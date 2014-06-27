f6945411d9d164fd052a516a97ade28ea81d9378
Jayesh Gohel <jayesh.gohel@gmail.com>
2014-06-27 20:07:28 +0530

    trying to add isotope

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 var app = angular.module("gnat", ['ngResource']);
 
 app.controller("listController", function($scope, $http){
+  $scope.$watch('works', function(n, o){
+    if(n != o) {
+      angular.forEach(n, function(value, key){
+        console.log(key);
+      });
+
+    }
+    // if(newVal.length > 0) console.log("loaded");
+  });
+
   $http({method: 'GET', url: 'works.json'}).success(function(data){
     $scope.works = data;
   });
+
+  $scope.sortBy = function(arg){
+    console.log(arg);
+    $(".list").isotope({ filter: '.featured' });
+  };
 });
 
 
 app.directive('isoGrid', function($log){
   return {
     link: function(scope, element, attrs){
-      element.isotope();
+      // element.isotope();
     }
   };
 
--- js/script.js
+++ js/script.js
@@ -6,61 +6,53 @@ $(function(){
 
   scroller = $('.box-wrap').antiscroll();
 
-  l.hover(function(){
-    w.addClass("active");
-  }, function(){
-    w.removeClass("active");
-  });
+  // l.hover(function(){
+  //   w.addClass("active");
+  // }, function(){
+  //   w.removeClass("active");
+  // });
 
-  // $(".works").isotope({
-  //   getSortData : {
-  //     title : function($elem){
-  //       return $elem.find('.title').text();
-  //     },
+  // $(".list").isotope({
 
-  //     year : function($elem){
-  //       return $elem.attr("data-year");
-  //     }
-  //   }
   // });
 
   // $(".works").isotope( { filter: "*", sortBy: "year title", sortAscending: true });
 
-  $(".element").click(function(e){
-    $(".element").removeClass("current");
-    $(".filter a").removeClass("active");
+  // $(".element").click(function(e){
+  //   $(".element").removeClass("current");
+  //   $(".filter a").removeClass("active");
     
-    $(this).addClass("current");
-    href = $(this).attr("data-href");
-    w.addClass("loading");
+  //   $(this).addClass("current");
+  //   href = $(this).attr("data-href");
+  //   w.addClass("loading");
 
-    $("#right iframe").load(function(){
-      w.removeClass("loading");
-    }).attr("src", href);
+  //   $("#right iframe").load(function(){
+  //     w.removeClass("loading");
+  //   }).attr("src", href);
 
-    var classes = $(this).attr("class").split(/\s+/);
-    $(".filter a").each(function(){
-      c = $(this).attr("href").slice(2);
-      if($.inArray(c, classes) !== -1) $(this).addClass("active");
-    });
-  });
+  //   var classes = $(this).attr("class").split(/\s+/);
+  //   $(".filter a").each(function(){
+  //     c = $(this).attr("href").slice(2);
+  //     if($.inArray(c, classes) !== -1) $(this).addClass("active");
+  //   });
+  // });
 
-  $("#left ul.sort a").click(function(e){
-    $("#left ul.sort a").removeClass("chosen");
-    $(this).addClass("chosen");
-    var type = $(this).attr('href').slice(1);
-    var asc = (type == "year") ? false : true;
-    $(".works").isotope({ sortBy: type, sortAscending: asc });
-    return false;
-  });
+  // $("#left ul.sort a").click(function(e){
+  //   $("#left ul.sort a").removeClass("chosen");
+  //   $(this).addClass("chosen");
+  //   var type = $(this).attr('href').slice(1);
+  //   var asc = (type == "year") ? false : true;
+  //   $(".works").isotope({ sortBy: type, sortAscending: asc });
+  //   return false;
+  // });
 
-  $("#left ul.filter a").click(function(e){
-    $("#left ul.filter a").removeClass("chosen");
-    $(this).addClass("chosen");
-    var type = $(this).attr('href').slice(1);
-    $(".works").isotope({ filter: type });
-    return false;
-  });
+  // $("#left ul.filter a").click(function(e){
+  //   $("#left ul.filter a").removeClass("chosen");
+  //   $(this).addClass("chosen");
+  //   var type = $(this).attr('href').slice(1);
+  //   $(".works").isotope({ filter: type });
+  //   return false;
+  // });
 
 
 
